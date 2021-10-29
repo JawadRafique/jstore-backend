9c67215cedcc5b8b94f1cadd5a4bbb5ae303ce88
Jawad Rafique <jawadrafique07@gmail.com>
2021-10-30 03:17:44 +0400

    welcome note changes

--- routes/welcomeNote.js
+++ routes/welcomeNote.js
@@ -4,9 +4,9 @@ router.get("/", (req, res) => {
     res.status(200).send({
         Developer: "Muhammad Jawad",
         version: "1",
-        Document: "Document will be available shortly",
+        Document: "Documentation about api will be available shortly",
         Github: "https://github.com/JawadRafique/jstore-backend",
-        LinkedIn: "https://www.linkedin.com/feed/",
+        LinkedIn: "https://www.linkedin.com/in/jawadrafique07/",
     });
 });
 
