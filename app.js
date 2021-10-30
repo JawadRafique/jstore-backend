34d789c02f549c7dae9b62bb50f08e0443a94a5d
Jawad Rafique <jawadrafique07@gmail.com>
2021-10-30 04:03:15 +0400

    origin changes

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://jstore-app.herokuapp.com/",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
+);
 
 // DB
 mongoose
