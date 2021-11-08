c3ba787877dbba55c6838e96fc7af58d590c1002
Jawad Rafique <jawadrafique07@gmail.com>
2021-11-08 20:22:37 +0400

    email functionality fixing

--- routes/mailFunction.js
+++ routes/mailFunction.js
@@ -1,31 +1,35 @@
 const router = require("express").Router();
+const nodemailer = require("nodemailer");
+
+// Configs
+const transporter = nodemailer.createTransport({
+    port: 465, // true for 465, false for other ports
+    host: "smtp.zoho.com",
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASS,
+    },
+    secure: true,
+});
+
+const mailOptions = {
+    from: "jawadrafique07@gmail.com",
+    to: "jawadrafique07@gmail.com",
+    subject: "Order Placed",
+    html: `<p>Order Placed</p>`,
+};
 
 // Mail
 router.post("/", (req, res) => {
-    const transporter = nodemailer.createTransport({
-        port: 465, // true for 465, false for other ports
-        host: "smtp.zoho.com",
-        auth: {
-            user: process.env.EMAIL_ID,
-            pass: process.env.EMAIL_PASS,
-        },
-        secure: true,
-    });
-    const mailOptions = {
-        from: "jawadrafique07@gmail.com",
-        to: "jawadrafique07@gmail.com",
-        subject: "Order Placed",
-        html: `<p>Order Placed</p>`,
-    };
     try {
         transporter.sendMail(mailOptions, function (err, info) {
             if (err)
                 res.status(400).send({
                     status: "Error in Mail function",
                 });
-            res.status(200).send({
-                status: "Email Sent Successfully",
-            });
+        });
+        res.status(200).send({
+            status: "Email Sent Successfully",
         });
     } catch {
         res.status(400).send({
