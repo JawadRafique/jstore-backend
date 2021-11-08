74d4855c70358fcdef8bdbd5960ab76d99efc848
Jawad Rafique <jawadrafique07@gmail.com>
2021-11-08 17:37:16 +0400

    order fixing

--- routes/order.js
+++ routes/order.js
@@ -19,7 +19,7 @@ router.post("/", verifyToken, async (req, res) => {
                 res.status(200).send(result);
             })
             .catch((err) => {
-                res.status(400).send("Required Field Missing");
+                res.status(400).send("Required Field Missing", err.message);
             });
     } catch {
         res.status(400).send({
